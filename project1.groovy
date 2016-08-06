820a4e06406f5b0827e944e3902b24d894ab6e8a
mlbit <mlbit@users.noreply.github.com>
2016-08-06 16:09:08 -0400

    Update project1.groovy

--- project1.groovy
+++ project1.groovy
@@ -2,19 +2,20 @@ def foldername = "playground"
 folder(foldername) {
     displayName("displayname")
     description("This is Description")
+    job('DSL-Tutorial-1-Test') {
+        scm {
+            git('git://github.com/mlbit/cicd-dsl-test.git')
+        }
+        triggers {
+            scm('H/15 * * * *')
+        }
+        steps {
+            maven('-e clean test')
+        }
+    }
 }
 
-job('DSL-Tutorial-1-Test') {
-    scm {
-        git('git://github.com/mlbit/cicd-dsl-test.git')
-    }
-    triggers {
-        scm('H/15 * * * *')
-    }
-    steps {
-        maven('-e clean test')
-    }
-}
+
 def project = 'mlbit/cicd-dsl-test'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
